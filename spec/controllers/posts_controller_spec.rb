c8e63cacc63e083d2c9a597d5f304afa7370304a
Edgar Russell <edgarrussell@gmail.com>
2017-04-12 22:39:57 -0500

    Used the resource generater for posts

---
+++ spec/controllers/posts_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+RSpec.describe PostsController, type: :controller do
+
+end
---
+++ spec/factories/posts.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :post do
+    date "2017-04-12"
+    rationale "MyText"
+  end
+end
